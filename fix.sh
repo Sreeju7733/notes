05a77a178925d9a94b491f71005df4d94fd84465
Sreeju <77568405+Sreeju7733@users.noreply.github.com>
2025-06-25 20:31:00 +0530

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -1,1 +1,2 @@
-sudo apt update && sudo apt install debootstrap live-build squashfs-tools grub-pc-bin grub-efi-bin mtools xorriso -y
+sudo apt update
+sudo apt install -y debootstrap live-build squashfs-tools grub-pc-bin grub-efi-bin mtools xorriso
