aad8bd65f84c3aaa40d0f4b468d72f57bf34a7d4
Sreeju <77568405+Sreeju7733@users.noreply.github.com>
2025-06-27 21:47:58 +0530

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -1,8 +1,6 @@
 sudo tee /home/sreeju/os1/archy-build/chroot/etc/apt/sources.list <<EOF
 deb http://deb.debian.org/debian sid main contrib non-free non-free-firmware
-deb http://security.debian.org/debian-security sid-security main
 EOF
-
-sudo cp /etc/resolv.conf /home/sreeju/os1/archy-build/chroot/etc/resolv.conf
-
 sudo chroot /home/sreeju/os1/archy-build/chroot
+apt update
+
